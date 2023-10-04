e365117e4162e42d2fe665386e3abde7f857af45
Viswanathan Krishnan <115349827+Viswanathan-04@users.noreply.github.com>
2023-10-04 21:29:47 +0530

    Day68-kdiffpairs.java

---
+++ Day68-kdiffpairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+        Set<Integer> s = new HashSet<>();
+
+        for(int num : nums) {
+            if(s.contains(num - k) || s.contains(num + k)) {
+                set.add(new ArrayList<>(Arrays.asList(num, num - k)));
+            } 
+            s.add(num);
+        }
+        return set.size();
+    }
+}
