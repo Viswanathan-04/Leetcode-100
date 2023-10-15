1c6f58da08779679bee1f5fd9d83926a7e95b277
Viswanathan Krishnan <115349827+Viswanathan-04@users.noreply.github.com>
2023-10-15 21:55:25 +0530

    Day79-lettercombi.java

---
+++ Day79-lettercombi.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return Collections.emptyList();
+
+        String[] phone_map = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> output = new ArrayList<>();
+        nextcomb("", digits, phone_map, output);
+        return output;
+    }
+
+    private void nextcomb(String combination, String next_digits, String[] phone_map, List<String> output) {
+        if (next_digits.isEmpty()) {
+            output.add(combination);
+        } else {
+            String letters = phone_map[next_digits.charAt(0) - '2'];
+            for (char letter : letters.toCharArray()) {
+                nextcomb(combination + letter, next_digits.substring(1), phone_map, output);
+            }
+        }
+    }
+}
